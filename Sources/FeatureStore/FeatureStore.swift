6160038169a4a49bf4d690658e60889533012b12
Serhan Aksüt <serhanaksut@gmail.com>
2023-09-30 17:24:15 +0200

    naming improvements have been done.

--- Sources/FeatureStore/FeatureStore.swift
+++ Sources/FeatureStore/FeatureStore.swift
@@ -2,24 +2,24 @@
 public final class FeatureStore {
     public static let shared = FeatureStore()
     
-    private var factories: [String: Any] = [:]
+    private var features: [String: Any] = [:]
     
     private func key<Feature>(_ type: Feature.Type) -> String {
         String(reflecting: type)
     }
     
-    public func register<Feature>(_ type: Feature.Type, factory: @escaping () -> Feature?) {
-        factories[key(type)] = factory
+    public func register<Feature>(_ type: Feature.Type, feature: @escaping () -> Feature?) {
+        features[key(type)] = feature
     }
     
     public func unregister<Feature>(_ type: Feature.Type) {
-        factories[key(type)] = nil
+        features[key(type)] = nil
     }
     
     public func resolve<Feature>(_ type: Feature.Type) -> Feature? {
-        guard let factory = factories[key(type)] as? () -> Feature? else {
+        guard let feature = features[key(type)] as? () -> Feature? else {
             return nil
         }
-        return factory()
+        return feature()
     }
 }
